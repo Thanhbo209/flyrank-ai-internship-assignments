028ce92022207a1602987f0811a8ca04e5a9cdc1
Thanh Pham <thanhagar123@gmail.com>
2026-07-22 22:54:52 +0700

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const TASKS = [
+  {
+    id: 1,
+    title: "Learn TypeScript",
+    done: true
+  },
+  {
+    id: 2,
+    title: "Learn Python",
+    done: false
+  },
+  {
+    id: 3,
+    title: "Learn Java",
+    done: false
+  },
+
+
+]
+
 app.get("/", (req, res) => {
   res.status(200).json({
     "name": "Task API",
@@ -15,6 +35,24 @@ app.get("/health", (req, res) => {
   res.json({
     "status": "ok"
   })
+})
+
+app.get("/tasks", (req, res) => {
+  res.status(200).json(TASKS);
+})
+
+app.get("/tasks/:id", (req, res) => {
+  const taskId = Number(req.params.id);
+
+  const task = TASKS.find((task) => task.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${taskId} not found`
+    })
+  }
+
+  res.status(200).json(task);
 })
 
 
