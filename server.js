6735fe061453b1433f93f3fd52aeeecb6babaf8b
Thanh Pham <thanhagar123@gmail.com>
2026-07-22 23:10:15 +0700

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
 const PORT = 3000;
 
 const TASKS = [
   {
     id: 1,
     title: "Learn TypeScript",
-    done: true
+    done: true,
   },
   {
     id: 2,
     title: "Learn Python",
-    done: false
+    done: false,
   },
   {
     id: 3,
     title: "Learn Java",
-    done: false
+    done: false,
   },
-
-
-]
+];
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    "name": "Task API",
-    "version": "1.0",
-    "endpoints": ["/tasks"]
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"],
   });
 });
 
 app.get("/health", (req, res) => {
   res.json({
-    "status": "ok"
-  })
-})
+    status: "ok",
+  });
+});
 
 app.get("/tasks", (req, res) => {
   res.status(200).json(TASKS);
-})
+});
 
 app.get("/tasks/:id", (req, res) => {
   const taskId = Number(req.params.id);
@@ -48,13 +48,32 @@ app.get("/tasks/:id", (req, res) => {
 
   if (!task) {
     return res.status(404).json({
-      error: `Task ${taskId} not found`
-    })
+      error: `Task ${taskId} not found`,
+    });
   }
 
   res.status(200).json(task);
-})
+});
 
+app.post("/tasks", (req, res) => {
+  const { title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({
+      error: "Title is missing!",
+    });
+  }
+
+  const createdTask = {
+    id: TASKS.length + 1,
+    title: title,
+    done: false,
+  };
+
+  TASKS.push(createdTask);
+
+  res.status(201).json(createdTask);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
